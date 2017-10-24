98baf36206eb4f00363da9577ee2fa3b076afcc4
Abhijit Bangera <abhijit.bangera@hotmail.com>
2017-10-24 16:58:11 +0530

    base setup

---
+++ web-presence/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abangerx\documents\github\web-presence\web-presence\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
