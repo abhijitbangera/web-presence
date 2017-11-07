dd913441e33a67c22d335975bbf5529556301c7d
Abhijit Bangera <abhijit.bangera@hotmail.com>
2017-11-07 23:27:55 +0530

    django auth base set

--- web-presence/mainProject/mainProject/settings.py
+++ web-presence/mainProject/mainProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',
+    # 'registration',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware', 
 ]
 
 ROOT_URLCONF = 'mainProject.urls'
@@ -63,10 +65,19 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+                'social_django.context_processors.backends', 
+                'social_django.context_processors.login_redirect',]
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+                            'social_core.backends.github.GithubOAuth2',
+                            'social_core.backends.twitter.TwitterOAuth',
+                            'social_core.backends.facebook.FacebookOAuth2',
+
+                            'django.contrib.auth.backends.ModelBackend',
+                        )
 
 WSGI_APPLICATION = 'mainProject.wsgi.application'
 
@@ -121,3 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ACCOUNT_ACTIVATION_DAYS = 0
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
--- web-presence/mainProject/mainProject/urls.py
+++ web-presence/mainProject/mainProject/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from mainProject import views as core_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.simple.urls')), 
+    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+    url(r'^admin/', admin.site.urls), 
 ]
