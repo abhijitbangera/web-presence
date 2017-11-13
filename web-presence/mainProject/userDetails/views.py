beb45a2caec373301d88fe4e13efb3447eca677c
Abhijit Bangera <abhijit.bangera@hotmail.com>
2017-11-14 00:33:57 +0530

    edit profile fixed

--- web-presence/mainProject/userDetails/views.py
+++ web-presence/mainProject/userDetails/views.py
@@ -10,13 +10,20 @@ class member():
 	def editMember(self,request):
 		user = get_user_model()
 		print ('user id is:',user.id)
-		my_record = memberDetails.objects.get(memberUserNumber=request.user.id)
-		form = memberDetailsForm(instance=my_record)
-		if form.is_valid():
-			save_it=form.save(commit = False)
-			save_it.memberRegistrationDate = datetime.datetime.now()
-			save_it.memberUserNumber=request.user
-			form.save()
+		form = memberDetailsForm()
+		if memberDetails.objects.filter(memberUserNumber=request.user.id).exists():	
+			my_record = memberDetails.objects.get(memberUserNumber=request.user.id)
+			form = memberDetailsForm(instance=my_record)
+			if request.method=='POST':
+				print ("yesssss")
+				print (request.POST)
+				memberDetails.objects.filter(memberUserNumber=request.user.id).update(\
+					memberName=request.POST['memberName'],memberGender=request.POST['memberGender'],memberLocation=request.POST['memberLocation'],\
+					memberContactNumber=request.POST['memberContactNumber'],memberOccupation=request.POST['memberOccupation'],\
+					memberAge=request.POST['memberAge'], memberEmail=request.POST['memberEmail']
+					)
+				my_record = memberDetails.objects.get(memberUserNumber=request.user.id)
+				form = memberDetailsForm(instance=my_record)
 
 		context={'form':form}
 		return render(request,'editMember.html',context=context)
