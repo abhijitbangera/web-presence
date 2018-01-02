5e1d0f67a50137af698c17cf2544393f166ccb25
Abhijit R Bangera <abangera@juniper.net>
2018-01-02 18:29:24 +0530

    minor changes in UI

--- web-presence/mainProject/mainProject/views.py
+++ web-presence/mainProject/mainProject/views.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import requests
+
+def get_stackOverFlow(user_id):
+	r = requests.get('https://api.stackexchange.com/2.2/users/'+user_id+'?order=desc&sort=reputation&site=stackoverflow')
+	print (r.text)
+	print ('---')
+	x=r.json()
+	print x
+	for i in x['items']:
+		rep=i['reputation']
+	print rep
+
+
 
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+get_stackOverFlow('4136116')
